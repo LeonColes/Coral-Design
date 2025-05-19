792d57f3eb2fc903d4c804a2d16ade7fafdb5157
Li Heng <133302251+LeonColes@users.noreply.github.com>
2025-05-20 01:41:29 +0800

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,5 +3,14 @@ import uni from '@uni-helper/eslint-config'
 export default uni(
   {
     unocss: true,
+    typescript: {
+      rules: {
+        '@typescript-eslint/no-explicit-any': 'error',
+        '@typescript-eslint/no-unsafe-assignment': 'error',
+        '@typescript-eslint/no-unsafe-member-access': 'error',
+        '@typescript-eslint/no-unsafe-call': 'error',
+        '@typescript-eslint/no-unsafe-return': 'error',
+      },
+    },
   },
 )
